e4e7d57cc2a422ee0bebcf907687efc0a0afecd4
sixzeroo <sixzeroo60@gmail.com>
2018-06-17 23:14:33 +0800

    update KMP

--- C++/028-1.cpp
+++ C++/028-1.cpp
@@ -1,3 +1,8 @@
+/*
+* 暴力解法
+*
+*/
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -21,5 +26,53 @@ public:
             }
         }
         return -1;
+    }
+};
+
+/*
+* KMP算法
+*/
+
+class Solution {
+public:
+    int strStr(string s, string p) {
+        int len = p.size();
+        if(len == 0) return 0;
+        // 生成next数组
+        vector<int> next(len, 0);
+        getNext(next, p);
+        // 两个数组的遍历指针
+        int i = 0, j = 0;
+        while(i < s.size() && j < len)
+        {
+            if(s[i] == p[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                if(j == 0) i++;
+                // 根据next数组中的信息进行重新指向
+                else j = next[j];
+            }
+        }
+        return j == len ? i - j : -1;
+    }
+    
+    void getNext(vector<int> &next, string p)
+    {
+        next[0] = -1;
+        // k表示最长前缀后缀的长度
+        int k = -1, i = 0;
+        // 计算到最后一位
+        while(i < p.size() - 1)
+        {
+            // 相匹配的时候对next数组赋值
+            if(k == -1 || p[i] == p[k])
+                next[++i] = ++k;
+            else
+                k = next[k];
+        }
     }
 };
